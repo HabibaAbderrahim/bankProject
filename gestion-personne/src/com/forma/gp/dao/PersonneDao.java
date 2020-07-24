94ae34ea5a6f8d3329989e2769db8557dce70f12
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-07-24 23:14:33 +0100

    save Personne using PreparedStatement

--- gestion-personne/src/com/forma/gp/dao/PersonneDao.java
+++ gestion-personne/src/com/forma/gp/dao/PersonneDao.java
@@ -1,13 +1,24 @@
 package com.forma.gp.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.forma.gp.entities.Personne;
+import com.forma.gp.utils.ConnexionUtil;
 
 public class PersonneDao {
 
-	public void save(Personne personne) {
+	public void save(Personne personne) throws ClassNotFoundException, SQLException {
+		String sql = "insert into personne(cin,nom,prenom,adresse,email) values(?,?,?,?,?)";
+		PreparedStatement statement = ConnexionUtil.getInstance().getConnection().prepareStatement(sql);
+		statement.setString(0, personne.getCin());
+		statement.setString(1, personne.getNom());
+		statement.setString(2, personne.getPrenom());
+		statement.setString(3, personne.getAdresse());
+		statement.setString(4, personne.getEmail());
 
+		statement.executeUpdate();
 	}
 
 	public void update(Personne personne) {
