fc6cc072c777e7e561b0a1bed8241ebbfa31118c
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-07-24 13:24:52 +0100

    Added Class PersonneDao.java <CRUD>

---
+++ gestion-personne/src/com/forma/gp/dao/PersonneDao.java
@@ -0,0 +1,19 @@
+package com.forma.gp.dao;
+
+import com.forma.gp.entities.Personne;
+
+public class PersonneDao {
+
+	public void save(Personne personne) {
+
+	}
+
+	public void update(Personne personne) {
+
+	}
+
+	public void delete(String cin) {
+
+	}
+
+}
