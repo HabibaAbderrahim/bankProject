e04a755f9cbf58a26e55fcd53852000d99091662
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-07-24 23:26:18 +0100

    Testing method save

--- gestion-personne/src/com/forma/gp/utils/Test.java
+++ gestion-personne/src/com/forma/gp/utils/Test.java
@@ -1,10 +1,17 @@
 package com.forma.gp.utils;
 
+import java.sql.SQLException;
+
+import com.forma.gp.dao.PersonneDao;
+import com.forma.gp.entities.Personne;
+
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		Personne pers = new Personne("0213458", "Habiba", "Abd", "Tunis", "heub@gmail.com");
+		PersonneDao personneDao = new PersonneDao();
+		personneDao.save(pers);
 	}
 
 }
