73ae6c4ba5f98887486ae4d128310d755d0872fe
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-07-24 13:18:42 +0100

    Added Java Bean Personne.java <entities>

--- gestion-personne/src/com/forma/gp/entities/Personne.java
+++ gestion-personne/src/com/forma/gp/entities/Personne.java
@@ -1,5 +1,67 @@
 package com.forma.gp.entities;
 
-public class Personne {
+import java.io.Serializable;
+
+public class Personne implements Serializable {
+	
+	private String cin;
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private String email;
+
+	public Personne() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Personne(String cin, String nom, String prenom, String adresse, String email) {
+		super();
+		this.cin = cin;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.email = email;
+	}
+
+	public String getCin() {
+		return cin;
+	}
+
+	public void setCin(String cin) {
+		this.cin = cin;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
