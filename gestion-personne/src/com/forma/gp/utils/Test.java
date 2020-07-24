413f4d0c893f0263e38e0fb4eb2095149348d881
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-07-25 00:00:48 +0100

    Testing delete personne2

--- gestion-personne/src/com/forma/gp/utils/Test.java
+++ gestion-personne/src/com/forma/gp/utils/Test.java
@@ -7,12 +7,35 @@ import com.forma.gp.entities.Personne;
 
 public class Test {
 
-	public static void main(String[] args)  {
-		
-		Personne pers = new Personne("0213458", "Habiba", "Abd", "Tunis", "heub@gmail.com");
+	public static void main(String[] args) {
+
 		PersonneDao personneDao = new PersonneDao();
+
+		// Personne1
+		/*
+		 * Personne pers = new Personne("0213458", "Habiba", "Abd", "Tunis",
+		 * "heub@gmail.com"); PersonneDao personneDao = new PersonneDao(); try {
+		 * personneDao.save(pers); } catch (ClassNotFoundException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); } catch (SQLException e) { //
+		 * TODO Auto-generated catch block e.printStackTrace(); }
+		 * 
+		 */
+		// Personne2
+		/*Personne pers0 = new Personne("0112458", "Hedi", "Ay", "Tunis", "hedi@gmail.com");
 		try {
-			personneDao.save(pers);
+			personneDao.save(pers0);
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}*/
+		// Update personne1
+		Personne pers = new Personne("0213458", "Habiba", "Abd", "Ariana", "heub@gmail.com");
+
+		try {
+			personneDao.update(pers);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -20,6 +43,19 @@ public class Test {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		//Delete Personne2
+		Personne pers0 = new Personne("0112458", "Hedi", "Ay", "Tunis", "hedi@gmail.com");
+		try {
+			personneDao.delete(pers0.getCin());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
