ffb9bfd1ea96c5f309ea8f4432c5a581d474bf68
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-07-25 01:41:01 +0100

    added findBycin , findByEmail and surcharged method FindByEmail

--- gestion-personne/src/com/forma/gp/dao/PersonneDao.java
+++ gestion-personne/src/com/forma/gp/dao/PersonneDao.java
@@ -52,20 +52,95 @@ public class PersonneDao {
 
 		String sql = "select * from personne";
 		PreparedStatement statement = ConnexionUtil.getInstance().getConnection().prepareStatement(sql);
-        //ResultSet like table 
+		// ResultSet like table
 		ResultSet result = statement.executeQuery();
 		List<Personne> list = new ArrayList<>();
-		//Add person in our return list
+		// Add person in our return list
 		while (result.next()) {
-            
-			//a chaque fois objet jdid
+
+			// a chaque fois objet jdid
 			Personne personne = new Personne();
 			personne.setCin(result.getString("cin"));
 			personne.setNom(result.getString("nom"));
 			personne.setPrenom(result.getString("prenom"));
 			personne.setAdresse(result.getString("adresse"));
 			personne.setEmail(result.getString("email"));
-            
+
+			list.add(personne);
+		}
+		return list;
+
+	}
+
+	public List<Personne> findByCin(String cin) throws ClassNotFoundException, SQLException {
+
+		String sql = "select * from personne where cin = ? ";
+		PreparedStatement statement = ConnexionUtil.getInstance().getConnection().prepareStatement(sql);
+		// ? bech bech ylawej
+		statement.setString(1, cin);
+		// ResultSet like table
+		ResultSet result = statement.executeQuery();
+		List<Personne> list = new ArrayList<>();
+
+		// Add person in our return list
+		while (result.next()) {
+
+			Personne personne = new Personne();
+			personne.setCin(result.getString("cin"));
+			personne.setNom(result.getString("nom"));
+			personne.setPrenom(result.getString("prenom"));
+			personne.setAdresse(result.getString("adresse"));
+			personne.setEmail(result.getString("email"));
+
+			list.add(personne);
+		}
+		return list ;
+	}
+
+	public List<Personne> findByEmail(String email) throws ClassNotFoundException, SQLException {
+
+		String sql = "select * from personne where email = ? ";
+		PreparedStatement statement = ConnexionUtil.getInstance().getConnection().prepareStatement(sql);
+		statement.setString(1, email);
+		// ResultSet like table
+		ResultSet result = statement.executeQuery();
+		List<Personne> list = new ArrayList<>();
+		// Add person in our return list
+		while (result.next()) {
+
+			Personne personne = new Personne();
+			personne.setCin(result.getString("cin"));
+			personne.setNom(result.getString("nom"));
+			personne.setPrenom(result.getString("prenom"));
+			personne.setAdresse(result.getString("adresse"));
+			personne.setEmail(result.getString("email"));
+
+			list.add(personne);
+		}
+		return list;
+
+	}
+
+	// surcharge
+	public List<Personne> findByEmail(String email, String cin) throws ClassNotFoundException, SQLException {
+
+		String sql = "select * from personne where email = ? and cin=? ";
+		PreparedStatement statement = ConnexionUtil.getInstance().getConnection().prepareStatement(sql);
+		statement.setString(1, email);
+		statement.setString(2, cin);
+		// ResultSet like table
+		ResultSet result = statement.executeQuery();
+		List<Personne> list = new ArrayList<>();
+		// Add person in our return list
+		while (result.next()) {
+
+			Personne personne = new Personne();
+			personne.setCin(result.getString("cin"));
+			personne.setNom(result.getString("nom"));
+			personne.setPrenom(result.getString("prenom"));
+			personne.setAdresse(result.getString("adresse"));
+			personne.setEmail(result.getString("email"));
+
 			list.add(personne);
 		}
 		return list;
