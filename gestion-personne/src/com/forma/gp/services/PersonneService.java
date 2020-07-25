e317721d6bd990f3f5725a9c89ee757ec7579217
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-07-25 14:15:51 +0100

    Added class PersonneService.java

---
+++ gestion-personne/src/com/forma/gp/services/PersonneService.java
@@ -0,0 +1,10 @@
+package com.forma.gp.services;
+
+import com.forma.gp.dao.PersonneDao;
+
+public class PersonneService {
+	
+	//where im getting my methods
+	private PersonneDao personneDao = new PersonneDao();
+
+}
