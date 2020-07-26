6d3a5cf610bfd331167760207f9cc4bc0ba0e281
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-07-26 21:10:02 +0100

    doPost and doGet methods

---
+++ gestion-personne/src/com/forma/gp/controllers/PersonneController.java
@@ -0,0 +1,30 @@
+package com.forma.gp.controllers;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/personneCtr")
+
+public class PersonneController extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+		
+	}
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+}
