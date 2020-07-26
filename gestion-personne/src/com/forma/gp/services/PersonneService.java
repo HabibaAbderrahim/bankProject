84295105c46ad2601e537113e6ffcc4b778a9487
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-07-26 20:26:22 +0100

    Added class PersonneController.java <javax.servlet.jar> <JSTL.jar>

--- gestion-personne/src/com/forma/gp/services/PersonneService.java
+++ gestion-personne/src/com/forma/gp/services/PersonneService.java
@@ -9,28 +9,67 @@ import com.forma.gp.model.MessageResponse;
 
 public class PersonneService {
 
-	// where im getting my methods
-	private PersonneDao personneDao = new PersonneDao();
+	public PersonneDao personneDao = new PersonneDao();
 
 	public MessageResponse save(Personne personne) throws ClassNotFoundException, SQLException {
-		// find by cin To test existance
-		// personne houwa eli ntesti 3lih
-		Personne pers = personneDao.findByCin(personne.getCin());
 
+		// pour verifier sans existance avant de lenregistrer
+		Personne pers = personneDao.findByCin(personne.getCin());
 		if (pers != null) {
 			return new MessageResponse(false, "CIN existant !!!");
 		}
 
-		// find by email To Test existance
 		List<Personne> list = personneDao.findByEmail(personne.getEmail());
 		if (!list.isEmpty()) {
 			return new MessageResponse(false, "Email existant");
-
 		}
-
 		personneDao.save(personne);
 		return new MessageResponse(true, "Operation effectuée avec succées");
 
+	}
+
+	public MessageResponse update(Personne personne) throws ClassNotFoundException, SQLException {
+
+		List<Personne> list = personneDao.findByEmail(personne.getEmail(), personne.getCin());
+
+		if (list.isEmpty()) {
+			
+			list = personneDao.findByEmail(personne.getEmail());
+		    if (!list.isEmpty()) {
+			return new MessageResponse(false, "Email existant");
+		}
+			
+			
+			
+			}
+		personneDao.update(personne);
+		return new MessageResponse(true, "Operation update effectuée avec succées");
+
+		}
+		
+
+
+	public MessageResponse delete(String cin) throws ClassNotFoundException, SQLException {
+		
+        //Tester sans existance
+		Personne pers = personneDao.findByCin(cin);
+
+		if (cin == null) {
+			return new MessageResponse(false, "CIN INTROUVABLE !!!");
+
+		}
+		personneDao.delete(cin);
+		return new MessageResponse(true, "Supprimm avec succés ");
+
+	}
+
+	public List<Personne> findAll() throws ClassNotFoundException, SQLException {
+		return personneDao.findAll();
+
+	}
+
+	public Personne findByCin(String cin) throws ClassNotFoundException, SQLException {
+		return personneDao.findByCin(cin);
 	}
 
 }
