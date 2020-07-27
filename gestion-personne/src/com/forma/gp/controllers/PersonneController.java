af3298d4bf1d766314608a36dfcbc4869a9c0eea
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-07-27 13:14:31 +0100

    add dispatcher

--- gestion-personne/src/com/forma/gp/controllers/PersonneController.java
+++ gestion-personne/src/com/forma/gp/controllers/PersonneController.java
@@ -3,11 +3,13 @@ package com.forma.gp.controllers;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Dispatch;
 
 import com.forma.gp.entities.Personne;
 import com.forma.gp.services.PersonneService;
@@ -18,6 +20,7 @@ public class PersonneController extends HttpServlet {
 
 	private Personne personne = new Personne();
 	private PersonneService personneService = new PersonneService();
+	private RequestDispatcher dispatcher;// page(navigation)
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -26,10 +29,14 @@ public class PersonneController extends HttpServlet {
 		String nom = req.getParameter("nom");
 		String prenom = req.getParameter("prenom");
 		String adresse = req.getParameter("adresse");
-		
+
 		personne = new Personne(cin, nom, prenom, adresse, email);
 		try {
+			
 			personneService.save(personne);
+			dispatcher = req.getRequestDispatcher("/AddPerson.jsp");
+			dispatcher.forward(req, resp);
+			
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
